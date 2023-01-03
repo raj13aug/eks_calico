c7904c8ceafb24077d058f7cf8ae7055465657e4
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-03 19:41:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,3 +103,19 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.test
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "Clusterip"
+  }
+}
